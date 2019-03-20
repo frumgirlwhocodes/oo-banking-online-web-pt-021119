675e788feb7fe0ebda1699375ef13cda06885c27
lieby weintraub <liebahinda@gmail.com>
2019-03-20 17:25:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,7 @@ def valid?
     sender.valid? && receiver.valid?
   end 
    def execute_transaction 
-     if valid? &&  sender.balance > amount && @status="pending"
+     if valid? &&  sender.balance > amount && self.status ="pending"
        sender.balance -= amount 
        receiver.balance += amount 
        @status= "complete"
