80051d4d612c10222a167565f7e35e7b66959f3b
lieby weintraub <liebahinda@gmail.com>
2019-03-20 17:26:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,9 +15,9 @@ def valid?
      if valid? &&  sender.balance > amount && self.status ="pending"
        sender.balance -= amount 
        receiver.balance += amount 
-       @status= "complete"
+       self.status= "complete"
      else 
-       @status= "rejected"
+       self.status= "rejected"
        "Transaction rejected. Please check your account balance."
    end 
  end 
